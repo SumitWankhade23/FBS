be8b8522af4912baecd26e74460e0cf004d9130b
Sumit Wankhade <wankhadesumit2390@gmail.com>
2026-08-13 18:16:32 +0530

    Delete Assignment 3 directory

--- Assignment 3/Q2.py
+++
@@ -1,7 +0,0 @@
-#Write a program to input any alphabet and check whether it is vowel or consonant.
-
-alpha = input("Enter alphabet: ")
-if alpha in 'aeiouAEIOU': 
-    print(f"{alpha} is vowel")
-else:
-    print(F"{alpha} is consonant")    
