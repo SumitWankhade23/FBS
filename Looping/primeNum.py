727556b94268340f843cc8798a611fa76d02d5c2
Sumit Wankhade <wankhadesumit2390@gmail.com>
2026-08-20 15:07:54 +0530

    Delete Looping directory
    Signed-off-by: Sumit Wankhade <wankhadesumit2390@gmail.com>

--- Looping/primeNum.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Enter number: "))
-for i in range(2,n):
-    print(n)
-    if (n%i == 0):
-        print(f"{n} is not prime number ")
-        break
-else:
-    print(f"{n} is prime number")    
-   
