d3f3a3bbb51dd4ca1798681ed5e59fa284e73640
Sumit Wankhade <wankhadesumit2390@gmail.com>
2026-08-23 17:29:02 +0530

    Modify Armstrong number program for range input
    Updated the program to find Armstrong numbers within a specified range instead of a single number.
    
    Signed-off-by: Sumit Wankhade <wankhadesumit2390@gmail.com>

--- Assignments/Assignment 5/Q4.py
+++ Assignments/Assignment 5/Q4.py
@@ -1,18 +1,23 @@
 #WAP to print Armstrong number within a given range
-num = int(input("Enter number: "))
-n = num
-total = 0
-count = 0
+start = int(input("Enter starting number: "))
+end = int(input("Enter ending number: "))
 
-while (n > 0):
-    count += 1
-    n = n//10
-n = num
-while(n > 0):
-    total = total + (n%10)**count
-    n = n // 10
+print("Armstrong numbers are:")
 
-if( num == total):
-    print(f"{num} is Armstrong number")
-else:
-    print(f"Is not Armstrong number")    
+for num in range(start, end + 1):
+
+    n = num
+    count = 0
+    total = 0
+
+    while (n > 0):
+        count += 1
+        n = n//10
+    n = num
+    while(n > 0):
+        total = total + (n%10)**count
+        n = n // 10
+
+    if( num == total):
+        print(f"{num} is Armstrong number")
+    
