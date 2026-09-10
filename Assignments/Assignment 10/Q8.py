f85943a10dadffa6e8e4c618c1d2542f39f0204b
Sumit Wankhade <wankhadesumit2390@gmail.com>
2026-09-10 19:33:27 +0530

    Implement list duplication function
    Added a function to duplicate a list without referencing the original.
    
    Signed-off-by: Sumit Wankhade <wankhadesumit2390@gmail.com>

--- Assignments/Assignment 10/Q8.py
+++ Assignments/Assignment 10/Q8.py
@@ -1,1 +1,11 @@
 #Write a program to create a duplicate of an existing list. It should not point to same list.
+def duplicate_list(data):
+    new_List = []
+    for i in range(len(data)):
+        new_List = new_List + [data[i]] 
+
+    return new_List  
+data = [10,20,30,40,50,50]
+result = duplicate_list(data)
+print("Old_List",data)
+print("New_List",result)  
