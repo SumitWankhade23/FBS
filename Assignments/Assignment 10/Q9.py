5c5b418af4146ac0b3e99a81a3d461d83e97b6fa
Sumit Wankhade <wankhadesumit2390@gmail.com>
2026-09-10 19:32:29 +0530

    Implement even and odd number separation in Q9.py
    Added a function to separate even and odd numbers from a list.
    
    Signed-off-by: Sumit Wankhade <wankhadesumit2390@gmail.com>

--- Assignments/Assignment 10/Q9.py
+++ Assignments/Assignment 10/Q9.py
@@ -1,3 +1,18 @@
 #Write a program of having n number of elements in the list and find out even
 # and odd elements in that list and then create two separate lists which will have
 # even elements and other will have odd elements.
+def check_num(data):
+    even_num = []
+    odd_num = []
+    for i in range(len(data)):
+        if data[i]%2 == 0:
+            even_num = even_num + [data[i]]
+        else:
+            odd_num = odd_num + [data[i]]
+    return odd_num, even_num        
+
+
+data = [11,2,3,4,5,6,4,7,8,78,25]
+result = check_num(data)
+print(f"Odd NUm List {result[0]}")
+print(f"Even NUm List {result[1]}")
