7389aec336f34171ea82cf6eedb5376a975ab07f
Sumit Wankhade <wankhadesumit2390@gmail.com>
2026-09-10 08:02:23 +0530

    Add files via upload
    Signed-off-by: Sumit Wankhade <wankhadesumit2390@gmail.com>

---
+++ Demos/Datastrucure/set/intro_set.py
@@ -0,0 +1,19 @@
+#1. { }
+s = set()
+s1 = {10,20,'ABCabc'}
+s2 = {10,20,30,40,50,60,20,10,30}
+print(s2)
+print(type(s))
+print(s1)
+
+#2. "Heterogeneous"
+
+#3. "Unordered"(No indexing avilable so )
+#No reapeting 
+
+#4. "Mutable", But Not editable(You can not change individual item) like s2[0]=80 rises error
+
+#5. "Unique", "so No Duplicate element"
+
+s1.add(70)
+print(s1)
---
+++ Demos/Datastrucure/set/set_methods.py
@@ -0,0 +1,35 @@
+s1 = {10,20,30,40,50,60,70}
+s2 = {30,40,51,52,53,55,60,60}
+s3 = {53,55}
+
+#s1.add(12)
+#s1.clear()
+#s4 = s2.copy()
+#print(s4)
+# s1.difference_update(s2)
+# print(s)
+# print(s1)
+# s1.discard(50)
+# s1.discard(20)
+# s1.discard(10)
+# print(s1)
+# print(s1.intersection(s2))
+# s1.intersection_update(s2)
+# print(s2)
+# print(s1.isdisjoint(s3))#True
+# print(s1.issubset(s2))#False
+# print(s3.issubset(s2))#True
+# print(s1.issuperset(s2))#False
+# print(s3.issuperset(s2))#False
+# print(s2.issuperset(s3))#True
+# s1.pop()
+# print(s1)
+# s1.remove(40)
+# print(s1)
+# s2.discard(51)
+# print(s2)
+# print(s1.symmetric_difference(s2))
+# s1.symmetric_difference_update(s2)
+#print(s1.union(s2))
+s1.update({71})
+print(s1)
