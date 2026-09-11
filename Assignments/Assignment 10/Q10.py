0703a4220ff5a992b1c60af0fd9bc90685808b47
Sumit Wankhade <wankhadesumit2390@gmail.com>
2026-09-11 09:43:47 +0530

    Implement function to remove specified element from list
    Signed-off-by: Sumit Wankhade <wankhadesumit2390@gmail.com>

--- Assignments/Assignment 10/Q10.py
+++ Assignments/Assignment 10/Q10.py
@@ -1,1 +1,17 @@
 #Write a program to remove all occurrences of a given element in the list.
+def remove_element(data, n):
+    new_data = []
+
+    for i in range(len(data)):
+        if data[i] != n:
+            new_data = new_data + [data[i]]
+
+    return new_data
+
+
+data = [10, 20, 20, 30, 40, 20, 50, 20, 60]
+print("Original List:", data)
+
+n = int(input("Enter element to be removed: "))
+result = remove_element(data, n)
+print("New List:", result)
