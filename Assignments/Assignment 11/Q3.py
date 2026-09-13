c0d3a5396c97d134fed06c26c5165445fe0b7639
Sumit Wankhade <wankhadesumit2390@gmail.com>
2026-09-13 21:34:14 +0530

    Implement sorting of list by second element
    Added sorting functionality for a list based on the second element of sublists, both with and without using a lambda function.
    
    Signed-off-by: Sumit Wankhade <wankhadesumit2390@gmail.com>

--- Assignments/Assignment 11/Q3.py
+++ Assignments/Assignment 11/Q3.py
@@ -1,1 +1,11 @@
 #Python Program to Sort the List According to the Second Element in Sublist
+data = [[1,50],[2,20],[3,40],[4,10]]
+data. sort(key=lambda x:x[1])
+print(data)
+
+##Without Lambda 
+def second_element(x):
+    return x[1]
+data =  [[1,50],[2,20],[3,40],[4,10]] 
+data.sort(key=second_element)
+print(data)
