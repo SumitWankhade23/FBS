be2d8fdbf253eda33b9230eb04e672d50e53e9c1
Sumit Wankhade <wankhadesumit2390@gmail.com>
2026-09-15 16:21:20 +0530

    Implement function to find second largest number
    Signed-off-by: Sumit Wankhade <wankhadesumit2390@gmail.com>

--- Assignments/Assignment 11/Q4.py
+++ Assignments/Assignment 11/Q4.py
@@ -1,1 +1,15 @@
 #Python Program to Find the Second Largest Number in a List Using Bubble Sort
+def secLarg_num(data):
+    max = 0
+    s_max = 0
+    for ind in range(1, len(data)):
+        if max < data[ind]:
+            s_max = max
+            max = data[ind]
+        elif(s_max < data[ind]):
+            s_max = data[ind]
+    return s_max
+        
+data = [10,20,30,99,45,78,98,40,50,60]
+result = secLarg_num(data)
+print(f"Second Larges Element: {result}")    
