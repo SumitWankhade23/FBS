d19d5e9cac170e0ebb4f0d35480004f1e235bbe9
Sumit Wankhade <wankhadesumit2390@gmail.com>
2026-09-17 16:26:38 +0530

    Add files via upload
    Signed-off-by: Sumit Wankhade <wankhadesumit2390@gmail.com>

---
+++ Test/Test6/Q1.py
@@ -0,0 +1,21 @@
+#Write a program which calculates toll calculation on some location following
+# is data provided:
+# Many vehicles goes through the toll every vehicle has to pay the basic
+# toll + extra charges if any.
+# two wheelers have to pay basic toll Rs 20 three wheelers have to pay
+# 30 and four wheelers have to pay 40
+# heavy veheicles i.e. Vehicles having wheels more than four, have to
+# pay 60 Rs as basic toll
+# extra charges :
+# for two wheelers if no. of persons are more than two extra charge
+# =10/person
+# for three wheelers if no. of persons are more than 3 extra charge
+# =20/person
+# for four wheelers if no. of persons are more than 4 extra charge
+# =40/person
+# for heavy vehicle if no. of person are more than 6 extra charges
+# =100/person.
+# Show polymorphic behaviour in main. Main module should be
+# designed in such way that toll should easily operate it through
+# interactive menu driven program .
+# Object of vehicle class should not be possible.
---
+++ Test/Test6/Solution.py
@@ -0,0 +1,119 @@
+class Vehicle:
+
+    def __init__(self):
+        if type(self) == Vehicle:
+            raise TypeError("Vehicle object cannot be created")
+
+    def calculate_toll(self):
+        print("Calculate toll")
+
+
+class TwoWheeler(Vehicle):
+
+    def __init__(self, persons):
+        super().__init__()
+        self.persons = persons
+
+    def calculate_toll(self):
+        toll = 20
+
+        if self.persons > 2:
+            extra_persons = self.persons - 2
+            toll = toll + extra_persons * 10
+
+        return toll
+
+
+class ThreeWheeler(Vehicle):
+
+    def __init__(self, persons):
+        super().__init__()
+        self.persons = persons
+
+    def calculate_toll(self):
+        toll = 30
+
+        if self.persons > 3:
+            extra_persons = self.persons - 3
+            toll = toll + extra_persons * 20
+
+        return toll
+
+
+class FourWheeler(Vehicle):
+
+    def __init__(self, persons):
+        super().__init__()
+        self.persons = persons
+
+    def calculate_toll(self):
+        toll = 40
+
+        if self.persons > 4:
+            extra_persons = self.persons - 4
+            toll = toll + extra_persons * 40
+
+        return toll
+
+
+class HeavyVehicle(Vehicle):
+
+    def __init__(self, persons):
+        super().__init__()
+        self.persons = persons
+
+    def calculate_toll(self):
+        toll = 60
+
+        if self.persons > 6:
+            extra_persons = self.persons - 6
+            toll = toll + extra_persons * 100
+
+        return toll
+
+
+# Main program
+
+while True:
+
+    print("\n========== TOLL MENU ==========")
+    print("1. Two Wheeler")
+    print("2. Three Wheeler")
+    print("3. Four Wheeler")
+    print("4. Heavy Vehicle")
+    print("5. Exit")
+
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+
+        persons = int(input("Enter number of persons: "))
+        vehicle = TwoWheeler(persons)
+
+    elif choice == 2:
+
+        persons = int(input("Enter number of persons: "))
+        vehicle = ThreeWheeler(persons)
+
+    elif choice == 3:
+
+        persons = int(input("Enter number of persons: "))
+        vehicle = FourWheeler(persons)
+
+    elif choice == 4:
+
+        persons = int(input("Enter number of persons: "))
+        vehicle = HeavyVehicle(persons)
+
+    elif choice == 5:
+
+        print("Thank you!")
+        break
+
+    else:
+
+        print("Invalid choice")
+        continue
+
+    # Polymorphism
+    print("Total Toll =", vehicle.calculate_toll())
