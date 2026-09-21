4a3ee52eb23698ebeed661967fa04931516ed458
Sumit Wankhade <wankhadesumit2390@gmail.com>
2026-09-21 18:41:29 +0530

    Implement intersection function for two lists
    Added a function to find the intersection of two lists and print the result.
    
    Signed-off-by: Sumit Wankhade <wankhadesumit2390@gmail.com>

--- Assignments/Assignment 11/Q7.py
+++ Assignments/Assignment 11/Q7.py
@@ -1,1 +1,15 @@
 #Python Program to Find the Intersection of Two Lists
+def intersec(data1,data2):
+    intersec_list = []
+    for i in data1:
+        if i in data2 and i not in intersec_list:
+            intersec_list.append(i)
+
+    return intersec_list
+
+data1 = [1,3,5,7,9,1]
+data2 = [2,4,6,8,10,1,3,5]
+result = intersec(data1,data2)
+print(f"data1 = {data1}")
+print(f"data2 = {data2}")
+print(f"Intersection of data1 and data2 : {result}")  
