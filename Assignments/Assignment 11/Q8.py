e4386cc407efa8b1a7388d114997a2280f00ffdf
Sumit Wankhade <wankhadesumit2390@gmail.com>
2026-09-21 18:40:57 +0530

    Add snake_ladder function to print 1 to 100
    Implement function to print numbers in a snakes and ladder pattern.
    
    Signed-off-by: Sumit Wankhade <wankhadesumit2390@gmail.com>

--- Assignments/Assignment 11/Q8.py
+++ Assignments/Assignment 11/Q8.py
@@ -1,1 +1,14 @@
 #Print 1 to 100 in snakes and ladder pattern.
+def snake_ladder():
+    for i in range(10,0,-1):
+        x = i * 10
+        data = []
+        for j in range(0,10):
+            data.append(x-j)
+        if(i % 2 != 0):
+            data.reverse()
+        for x in data:    
+            print(x,end="\t")
+        print()
+             
+snake_ladder()
