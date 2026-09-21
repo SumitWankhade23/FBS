aeea782d24ec556b5be83c7021df2f9172f31ca5
Sumit Wankhade <wankhadesumit2390@gmail.com>
2026-09-21 18:42:39 +0530

    Implement selection sort for list sorting by length
    Signed-off-by: Sumit Wankhade <wankhadesumit2390@gmail.com>

--- Assignments/Assignment 11/Q5.py
+++ Assignments/Assignment 11/Q5.py
@@ -1,2 +1,17 @@
 #Python Program to Sort a List According to the Length of the Elements
 #within the list.
+def selectionsort(data):
+    size = len(data)
+    for i in range(0,size-1):
+        index = i
+        for j in range(i+1,size):
+            if(len(data[j]) < len(data[index])):
+                index = j
+        data[i],data[index] = data[index],data[i]
+        #print(data)
+
+
+data = ["Apple","I","Egg","Pineapple","Sanjana","Go"] 
+print('Before sorting: ',data)
+selectionsort(data)
+print('After sorting: ',data)
