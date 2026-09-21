32badc23a26f0b5c138a0d3c9cb4a7b5aa244690
Sumit Wankhade <wankhadesumit2390@gmail.com>
2026-09-21 18:39:54 +0530

    Implement function to remove even numbers from list
    Added a function to remove even numbers from a list and print the new list.
    
    Signed-off-by: Sumit Wankhade <wankhadesumit2390@gmail.com>

--- Assignments/Assignment 11/Q10.py
+++ Assignments/Assignment 11/Q10.py
@@ -1,1 +1,11 @@
 #10. Write a program to print list after removing even numbers.
+def remov_evennum(data):
+    li = []
+    for i in data:
+        if i % 2 != 0:
+            li.append(i)
+    return li
+
+data = [1,2,3,4,5,6,7,8]
+result = remov_evennum(data)
+print(f"New list = {result}")
