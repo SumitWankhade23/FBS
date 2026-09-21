d7a1835fb7c85774acb655821750590a2a8d4790
Sumit Wankhade <wankhadesumit2390@gmail.com>
2026-09-21 18:40:28 +0530

    Implement function to generate squares and cubes
    Added a function to create lists of squares and cubes from a given list of numbers.
    
    Signed-off-by: Sumit Wankhade <wankhadesumit2390@gmail.com>

--- Assignments/Assignment 11/Q9.py
+++ Assignments/Assignment 11/Q9.py
@@ -1,1 +1,14 @@
 #Write a program to create three lists of numbers, their squares and cubes
+def create_list(data):
+    squr_list = []
+    cub_list = []
+    for i in data:
+        squr_list.append(i**2)
+        cub_list.append(i**3)
+
+    return squr_list,cub_list
+data = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+result = create_list(data)
+print(f"List = {data}")
+print(f"Squre list = {result[0]}")
+print(f"Cube list = {result[1]}")    
