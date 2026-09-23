c47bfbce4be0dfacaaaa0431316c6c868042931c
Sumit Wankhade <wankhadesumit2390@gmail.com>
2026-09-23 22:35:14 +0530

    Update Q5.py
    Signed-off-by: Sumit Wankhade <wankhadesumit2390@gmail.com>

--- Assignments/Assignment 12/Q5.py
+++ Assignments/Assignment 12/Q5.py
@@ -1,1 +1,12 @@
 #5. Python Program to Count the Number of Vowels in a String
+def count_vovel(text):
+    vov = 'aeiou'
+    total = 0
+    for char in text.lower():
+        if char in vov:
+            total = total +  1
+    return total
+
+text = "Mere brother ki dulhan"
+result = count_vovel(text)
+print(result)
