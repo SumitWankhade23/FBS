7c898f003a866b642c0e84e7bcc9e7ba415b1758
Sumit Wankhade <wankhadesumit2390@gmail.com>
2026-09-23 09:19:12 +0530

    Implement function to swap first and last characters
    Signed-off-by: Sumit Wankhade <wankhadesumit2390@gmail.com>

--- Assignments/Assignment 12/Q4.py
+++ Assignments/Assignment 12/Q4.py
@@ -1,2 +1,16 @@
 #4. Python Program to Form a New String where the First Character and
 #   he Last Character have been Exchanged
+def swap_first_last(text):
+    if len(text) <= 1:
+        return text
+
+    new_text = text[-1]              
+    for i in range(1, len(text) - 1):
+        new_text = new_text + text[i]   
+    new_text = new_text + text[0]    
+
+    return new_text
+
+
+text = str(input("Enter text: "))
+print(swap_first_last(text))
