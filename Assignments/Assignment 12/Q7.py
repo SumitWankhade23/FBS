4a88d81eda9b06fe62d1eea3d66bf0f9f4649339
Sumit Wankhade <wankhadesumit2390@gmail.com>
2026-09-23 22:36:38 +0530

    Implement string length calculation function
    Added a function to calculate the length of a string without using library functions.
    
    Signed-off-by: Sumit Wankhade <wankhadesumit2390@gmail.com>

--- Assignments/Assignment 12/Q7.py
+++ Assignments/Assignment 12/Q7.py
@@ -1,2 +1,12 @@
 #7. Python Program to Calculate the Length of a String Without Using a
 #   Library Function
+def cal_Lenstring(text):
+    count = 0
+    for char in text:
+        count += 1
+    return count
+
+text = "Firsthbit solution"
+result = cal_Lenstring(text)
+print(result)        
+            
