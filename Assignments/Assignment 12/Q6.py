b1fd251d116a6f2e2aeeac78cb8b84a2f5bc51d0
Sumit Wankhade <wankhadesumit2390@gmail.com>
2026-09-23 22:36:05 +0530

    Implement string space replacement function
    Added a function to replace spaces with hyphens in a string.
    
    Signed-off-by: Sumit Wankhade <wankhadesumit2390@gmail.com>

--- Assignments/Assignment 12/Q6.py
+++ Assignments/Assignment 12/Q6.py
@@ -1,2 +1,20 @@
 #6. Python Program to Take in a String and Replace Every Blank Space
 #   with Hyphen
+def reap_string(text):
+    result = ""
+    for char in text:
+        if char == " ":
+            result += "-"
+        else:
+            result +=  char
+    return result
+
+text = "Mere Brother Ki Dulhan"
+new_text = reap_string(text)
+print(new_text)            
+
+ 
+#shortest version 
+# text = "Mere brother ki dulhan"
+# new_text = text.replace(' ', '-')
+# print(new_text)    
