ddb79a88989da0329c6df211bcad087331ba8e53
Sumit Wankhade <wankhadesumit2390@gmail.com>
2026-09-23 22:37:24 +0530

    Implement function to remove odd index characters
    Added a function to remove characters at odd indices and provided an alternative approach using slicing.
    
    Signed-off-by: Sumit Wankhade <wankhadesumit2390@gmail.com>

--- Assignments/Assignment 12/Q8.py
+++ Assignments/Assignment 12/Q8.py
@@ -1,2 +1,18 @@
 #8. Python Program to Remove the Characters of Odd Index Values in a
 #   String
+def rem_oddindexchar(text):
+    new_text = ""
+    for i in range(len(text)):
+        if i %2 == 0:
+            new_text += text[i]
+    return new_text
+
+text = "Sumit"
+result = rem_oddindexchar(text)
+print(result)    
+
+#Two approch 
+def rem_oddindexchar(text):
+    return text[::2] 
+  
+print(rem_oddindexchar("Elon"))
