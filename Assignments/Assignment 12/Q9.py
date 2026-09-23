82b2b004d31c3cc6624d7fa5ddd3ab63b332606d
Sumit Wankhade <wankhadesumit2390@gmail.com>
2026-09-23 22:37:58 +0530

    Implement character and word count function
    Added a function to calculate character and word counts in a string.
    
    Signed-off-by: Sumit Wankhade <wankhadesumit2390@gmail.com>

--- Assignments/Assignment 12/Q9.py
+++ Assignments/Assignment 12/Q9.py
@@ -1,2 +1,24 @@
 # 9. Python Program to Calculate the Number of Words and the Number of
 # Characters Present in a String
+def cal_charNum(data):
+    chr_count = 0
+    words_count = 0
+    in_word = False
+    for ch in data:
+        if ch != " ":
+            chr_count = chr_count + 1
+        
+            if in_word == False:
+                words_count = words_count + 1
+                in_word = True
+        else:
+            in_word = False
+    
+    return chr_count,words_count
+
+data = str(input("Enter string: "))
+result = cal_charNum(data)
+print(f"Charechter couunt = {result[0]}") 
+print(f"Word count = {result[1]}")           
+
+
