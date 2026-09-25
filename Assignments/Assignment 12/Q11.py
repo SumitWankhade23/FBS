ed10c239e4bc48be9c73d14823cf5a5aac70aad8
Sumit Wankhade <wankhadesumit2390@gmail.com>
2026-09-25 18:35:34 +0530

    Implement function to replace spaces with hyphens
    Signed-off-by: Sumit Wankhade <wankhadesumit2390@gmail.com>

--- Assignments/Assignment 12/Q11.py
+++ Assignments/Assignment 12/Q11.py
@@ -1,1 +1,13 @@
 # 11. Python Program to replace every blank space with hyphen in a string.
+def replace_blank(data):
+    new_data = ""
+    for ch in data:
+        if ch == " ":
+            new_data += "-"
+        else:
+            new_data += ch
+    return new_data
+
+data = "Elon musk has X"
+result = replace_blank(data)
+print(result)            
