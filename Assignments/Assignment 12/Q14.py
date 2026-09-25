abdf1ca52d129dca07bc7e24d52953c694a4a037
Sumit Wankhade <wankhadesumit2390@gmail.com>
2026-09-25 18:37:30 +0530

    Implement word counting function in
    Signed-off-by: Sumit Wankhade <wankhadesumit2390@gmail.com>

--- Assignments/Assignment 12/Q14.py
+++ Assignments/Assignment 12/Q14.py
@@ -1,1 +1,15 @@
 # 14. Python Program to count the occurrences of ach word in a string.
+def count_words(text):
+    word_count = {}
+    words = text.split()
+    for word in words:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+    return word_count
+
+
+text = "python is easy and python is powerful"
+result = count_words(text)
+print(result)
