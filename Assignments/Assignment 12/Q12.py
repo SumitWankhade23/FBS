bc815baaf33db0908acba07f2c7e32474afcdca9
Sumit Wankhade <wankhadesumit2390@gmail.com>
2026-09-25 18:36:02 +0530

    Implement function to count lowercase characters
    Signed-off-by: Sumit Wankhade <wankhadesumit2390@gmail.com>

--- Assignments/Assignment 12/Q12.py
+++ Assignments/Assignment 12/Q12.py
@@ -1,1 +1,12 @@
 # 12. Python Program to count number of lowercase characters in a string.
+def count_lowercase(text):
+    count = 0
+    for ch in text:
+        if 'a' <= ch <= 'z':
+            count += 1
+    return count
+
+text = str(input("Enter string: "))
+result = count_lowercase(text) 
+print(result)       
+
