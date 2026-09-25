377b55578b7ba52e9e8c21426cfc3f1ca56fa596
Sumit Wankhade <wankhadesumit2390@gmail.com>
2026-09-25 18:36:27 +0530

    Implement digit and letter counting function
    Added a function to count digits and letters in a string.
    
    Signed-off-by: Sumit Wankhade <wankhadesumit2390@gmail.com>

--- Assignments/Assignment 12/Q13.py
+++ Assignments/Assignment 12/Q13.py
@@ -1,1 +1,14 @@
 # 13. Python Program to count number of digits and letters in a string.
+def count_digits_letters(text):
+    dig_count = 0
+    let_count = 0
+    for ch in text:
+        if "a" <= ch <= "z" or "A" <= ch <= "Z":
+            let_count += 1
+        elif "0" <= ch <= "9":
+            dig_count += 1
+    return dig_count,let_count 
+text = "Sumit23"
+result = count_digits_letters(text)
+print(result)
+           
