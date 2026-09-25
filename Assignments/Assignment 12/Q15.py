71bffee1e951c9a4690dcfe2703addab023e9187
Sumit Wankhade <wankhadesumit2390@gmail.com>
2026-09-25 18:38:24 +0530

    Implement string length comparison functions
    Added functions to compare the lengths of two strings without using built-in functions.
    
    Signed-off-by: Sumit Wankhade <wankhadesumit2390@gmail.com>

--- Assignments/Assignment 12/Q15.py
+++ Assignments/Assignment 12/Q15.py
@@ -1,1 +1,23 @@
 # 15. Python Program to find larger string without using built-in functions.
+def string_length(text):
+    count = 0
+    for char in text:
+        count += 1
+    return count
+
+def Larger_string(text1,text2):
+    Len1 = string_length(text1) 
+    Len2 = string_length(text2) 
+    if Len1 > Len2:
+        print(f"FirstString = {text1} is Larger string")
+    elif Len2 > Len1:
+        print(f"SecondString = {text2} is Larger string")
+    else:
+        print(f"text1 and text2 are equal length string")
+        
+
+text1 = str(input("Enter string1: ")) 
+text2 = str(input("Enter string2: "))
+Larger_string(text1,text2)
+  
+
