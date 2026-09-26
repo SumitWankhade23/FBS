6330f12c981a30ed9adde831e9b1792d6412c1b6
Sumit Wankhade <wankhadesumit2390@gmail.com>
2026-09-26 17:53:29 +0530

    Implement function to multiply dictionary items
    Signed-off-by: Sumit Wankhade <wankhadesumit2390@gmail.com>

--- Assignments/Assignment 13/Q6.py
+++ Assignments/Assignment 13/Q6.py
@@ -1,1 +1,12 @@
 # 6. Python Program to Multiply All the Items in a Dictionary
+def multiply_all(data):
+    total = 1
+    for i in data.values():
+        total *= i
+    return total
+
+data = {"A": 10, "B":20, "C":30, "D":40, "E":50}
+result = multiply_all(data)
+print(result)   
+
+
