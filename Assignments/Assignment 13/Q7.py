ec926af9795e97b4209a7bab162021847a09dc14
Sumit Wankhade <wankhadesumit2390@gmail.com>
2026-09-26 17:56:12 +0530

    Implement function to remove key from dictionary
    Signed-off-by: Sumit Wankhade <wankhadesumit2390@gmail.com>

--- Assignments/Assignment 13/Q7.py
+++ Assignments/Assignment 13/Q7.py
@@ -1,1 +1,11 @@
 # 7. Python Program to Remove the Given Key from a Dictionary
+def remove_key(student,key):
+        if key in student:
+            del student[key]
+            print("Output:", student)
+        else:
+            print(f"'{key}' not found in the dictionary")
+
+student = {"Name": "Virat", "Salary":125000, "Age": 35, "company": "HCL"}
+key = input("Enter key to remove: ")
+remove_key(student,key)
