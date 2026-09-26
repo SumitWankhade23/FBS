04e4f940d4beee7e7edff0ad433f02de921eb496
Sumit Wankhade <wankhadesumit2390@gmail.com>
2026-09-26 17:58:04 +0530

    Implement word frequency counter in Q8.py
    Added a function to count word frequency and a shorter approach using Counter.
    
    Signed-off-by: Sumit Wankhade <wankhadesumit2390@gmail.com>

--- Assignments/Assignment 13/Q8.py
+++ Assignments/Assignment 13/Q8.py
@@ -1,2 +1,20 @@
 # 8. Python Program to Count the Frequency of Words Appearing in a String Using
-# a Dictionary
+#    a Dictionary
+def word_appear(text):
+    freq = {}
+    words = text.split()          
+    for word in words:
+        if word in freq:
+            freq[word] += 1
+        else:
+            freq[word] = 1
+    return freq
+  
+text = "Two One Two Fourty One Three One"
+result = word_appear(text)
+print(result)
+
+#Shorter approch 
+from collections import Counter
+text = "Two One Two Fourty One Three One"
+print(Counter(text.split()))
