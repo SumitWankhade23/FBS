c22d91d868e340b946740e26ec521490631a0c5d
Sumit Wankhade <wankhadesumit2390@gmail.com>
2026-09-26 17:53:01 +0530

    Implement function to sum dictionary values
    Signed-off-by: Sumit Wankhade <wankhadesumit2390@gmail.com>

--- Assignments/Assignment 13/Q5.py
+++ Assignments/Assignment 13/Q5.py
@@ -1,1 +1,11 @@
 # 5. Python Program to Sum All the Items in a Dictionary
+def sum_all(data):
+    total = 0
+    for i in data.values():
+        total += i
+    return total
+
+data = {"A": 10, "B":20, "C":30, "D":40, "E":50}
+result = sum_all(data)
+print(result)   
+
